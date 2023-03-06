898fca1abbe63ee2a9221174a335dafe0463a70d
Sava Cristian Andrei <savacristianandrei@yahoo.com>
2023-03-06 11:31:04 +0200

    Lab 3

---
+++ TSC_Lab3/lab01_testbench-interface/top.sv
@@ -0,0 +1,66 @@
+/***********************************************************************
+ * A SystemVerilog top-level netlist to connect testbench to DUT
+ **********************************************************************/
+
+module top;
+  timeunit 1ns/1ns; //seteaza timpul de simulare 
+
+  // user-defined types are defined in instr_register_pkg.sv
+  import instr_register_pkg::*;
+
+  // clock variables
+  logic clk;
+  logic test_clk; 
+  //logic = 4 stari 0,1,x,z p/pe un bit
+
+  // interconnecting signals
+  logic          load_en; // load_enable
+  logic          reset_n; //reset_negative
+  opcode_t       opcode;   
+  operand_t      operand_a, operand_b;
+  address_t      write_pointer, read_pointer;
+  instruction_t  instruction_word;
+
+  // instantiate testbench and connect ports
+  instr_register_test test (
+    .clk(test_clk),
+    .load_en(load_en),
+    .reset_n(reset_n),
+    .operand_a(operand_a),
+    .operand_b(operand_b),
+    .opcode(opcode),
+    .write_pointer(write_pointer),
+    .read_pointer(read_pointer),
+    .instruction_word(instruction_word)
+   );
+
+  // instantiate design and connect ports
+  instr_register dut (
+    .clk(clk),
+    .load_en(load_en),
+    .reset_n(reset_n),
+    .operand_a(operand_a),
+    .operand_b(operand_b),
+    .opcode(opcode),
+    .write_pointer(write_pointer),
+    .read_pointer(read_pointer),
+    .instruction_word(instruction_word)
+   );
+
+  // clock oscillators
+  initial begin
+    clk <= 0;
+    forever #5  clk = ~clk;
+  end
+
+  initial begin
+    test_clk <=0;
+    // offset test_clk edges from clk to prevent races between
+    // the testbench and the design
+    #4 forever begin
+      #2ns test_clk = 1'b1;
+      #8ns test_clk = 1'b0;
+    end
+  end
+
+endmodule: top
